e7b4619082471e91dafb0c051aa84571c21c4101
panicbit <panicbit.dev@gmail.com>
2022-07-18 15:50:02 +0200

    add appid types

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::path::PathBuf;
 use anyhow::Result;
 use crc::{Crc, CRC_32_ISO_HDLC};
 
+use crate::steam::id::LegacyAppId;
+
 mod steam;
 
 fn main() -> Result<()> {
@@ -16,31 +18,10 @@ fn main() -> Result<()> {
     println!("{:#?}", shortcuts);
 
     let name = "foo";
-    let path = "bar.exe";
-    let legacy_app_id = generate_legacy_app_id(name, path);
-    dbg!(legacy_app_id);
-    let shortcut_id = generate_app_id(name, path);
-    dbg!(shortcut_id);
+    let target = "bar.exe";
+    let legacy_app_id = LegacyAppId::from_shortcut_name_and_target(name, target);
+
     Ok(())
-}
-
-fn generate_legacy_app_id(name: &str, path: &str) -> u64 {
-    let crc = Crc::<u32>::new(&CRC_32_ISO_HDLC);
-    let mut digest = crc.digest();
-
-    digest.update(path.as_bytes());
-    digest.update(name.as_bytes());
-
-    let mut id = digest.finalize() as u64;
-    id |= 0x80000000;
-    id <<= 32;
-    id |= 0x02000000;
-
-    id
-}
-
-fn generate_app_id(name: &str, path: &str) -> u32 {
-    (generate_legacy_app_id(name, path) >> 32) as u32
 }
 
 fn get_shortcuts_vdf_path() -> PathBuf {
